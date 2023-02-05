ab314e74c3a431c0c002793f85290260decf9767
KYapp <kukiyomenaisapp@gmail.com>
2023-02-05 22:55:59 +0900

    Grid

---
+++ Assets/popoInventory/Runtime/Grid.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace JuhaKurisu.PopoTools.InventorySystem
+{
+    public class Grid<ItemType>
+    {
+        public ItemType item
+        {
+            get => amount > 0 ? _item : getEmptyItem.Invoke();
+            private set => _item = value;
+        }
+        ItemType _item;
+        public int amount
+        {
+            get => _amount;
+            set => _amount = Math.Clamp(value, 0, maxAmount);
+        }
+        int _amount;
+        public int maxAmount => getMaxAmount.Invoke(item);
+        event Func<ItemType, int> getMaxAmount;
+        event Func<ItemType> getEmptyItem;
+
+        public Grid(ItemType item, Func<ItemType, int> getMaxAmount, Func<ItemType> getEmptyItem)
+        {
+            this.item = item;
+            amount = 0;
+            this.getMaxAmount = getMaxAmount;
+            this.getEmptyItem = getEmptyItem;
+        }
+    }
+}
