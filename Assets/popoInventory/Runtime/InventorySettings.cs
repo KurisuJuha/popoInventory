e3288d49d151fa72c8f61c14090055c4262c57b8
Juha <kukiyomenaisapp@gmail.com>
2023-05-17 13:47:06 +0900

    InventorySettingsを定義

---
+++ Assets/popoInventory/Runtime/InventorySettings.cs
@@ -0,0 +1,57 @@
+namespace JuhaKurisu.PopoTools.InventorySystem
+{
+    public class InventorySettings<TItem> : IInventorySettings<TItem>
+    {
+        public delegate TItem CopyItemCallback();
+
+        public delegate TItem GenerateEmptyItemCallback();
+
+        public delegate bool IsEmptyItemCallback(TItem item);
+
+        public delegate bool IsSameItemCallback(TItem itemA, TItem itemB);
+
+        public delegate int ItemMaxAmountCallback(TItem item);
+
+        private readonly CopyItemCallback copyItem;
+        private readonly GenerateEmptyItemCallback generateEmptyItem;
+        private readonly IsEmptyItemCallback isEmptyItem;
+        private readonly IsSameItemCallback isSameItem;
+        private readonly ItemMaxAmountCallback itemMaxAmount;
+
+        public InventorySettings(CopyItemCallback copyItem, IsSameItemCallback isSameItem,
+            IsEmptyItemCallback isEmptyItem, ItemMaxAmountCallback itemMaxAmount,
+            GenerateEmptyItemCallback generateEmptyItem)
+        {
+            this.copyItem = copyItem;
+            this.isSameItem = isSameItem;
+            this.isEmptyItem = isEmptyItem;
+            this.itemMaxAmount = itemMaxAmount;
+            this.generateEmptyItem = generateEmptyItem;
+        }
+
+        public TItem CopyItem()
+        {
+            return copyItem.Invoke();
+        }
+
+        public bool IsSameItem(TItem itemA, TItem itemB)
+        {
+            return isSameItem.Invoke(itemA, itemB);
+        }
+
+        public bool IsEmptyItem(TItem item)
+        {
+            return isEmptyItem.Invoke(item);
+        }
+
+        public int ItemMaxAmount(TItem item)
+        {
+            return itemMaxAmount.Invoke(item);
+        }
+
+        public TItem GenerateEmptyItem()
+        {
+            return generateEmptyItem.Invoke();
+        }
+    }
+}
