9c8b3af227e40d89fcb7d8c76ae41d61a5154b06
KYapp <kukiyomenaisapp@gmail.com>
2023-06-17 13:35:00 +0900

    testのセットアップ

---
+++ Test/InventoryGridTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Test;
+
+public class InventoryGridTests
+{
+    [Test]
+    public void IsAddableItem_アイテムが一つもないGridにアイテムを足す_問題なく足せる()
+    {
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+}
---
+++ Test/TestItem.cs
@@ -0,0 +1,30 @@
+﻿namespace Test;
+
+public sealed class TestItem : IEquatable<TestItem>
+{
+    public readonly string Value;
+
+    public TestItem(string value)
+    {
+        Value = value;
+    }
+
+    public bool Equals(TestItem? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Value == other.Value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == GetType() && Equals((TestItem)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
+}
